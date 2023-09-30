13a283c605366bb89035288aa8b07e14f876bcef
Franz Eugenio <franze2019@gmail.com>
2023-09-30 00:45:20 -0700

    Connected backend to DB

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+// Express app
+const app = express()
+
+// Middlware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// Routes
+app.use('/api/workouts/', workoutRoutes)
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
