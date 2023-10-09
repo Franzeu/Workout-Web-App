aee3d76da52e32886c05cc7710f3ec31538c09dd
Franz Eugenio <franze2019@gmail.com>
2023-10-09 16:02:24 -0700

    Updated routes. Added controllers and models

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+// Defines the structure of our database
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    load: {
+        type: Number,
+        required: true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Workout', workoutSchema)
+
